ec584565d08184a0354a714f6a2f946ceea23da7
DoGooder9000 <mike@rogness.net>
2025-07-26 23:08:40 -0500

    Added more bitboard stuff

--- src/bitboard.c
+++ src/bitboard.c
@@ -1,1 +1,19 @@
 #include "include/bitboard.h"
+
+static const U64 IsAFile = 0x0101010101010101;
+static const U64 IsHFile = 0x1010101010101010;
+static const U64 IsBFile = IsAFile << 1;
+static const U64 IsGFile = IsHFile >> 1;
+
+static const U64 IsABFile = IsAFile | IsBFile;
+static const U64 IsGHFile = IsGFile | IsHFile;
+
+static const U64 IsRank8 = 0x00000000000000FF;
+static const U64 IsRank7 = 0x000000000000FF00;
+static const U64 IsRank2 = 0x00FF000000000000;
+static const U64 IsRank1 = 0xFF00000000000000;
+
+U64 PrecomputedPawnAttacks[2][64];
+U64 PrecomputedPawnMoves[2][64];
+U64 PrecomputedKnightMoves[64];
+U64 PrecomputedKingMoves[64];
--- src/include/bitboard.h
+++ src/include/bitboard.h
@@ -6,3 +6,15 @@
 #define SetBit(b, i) ((b) |= (1ULL << (i)))
 #define ClearBit(b, i) ((b) &= ~(1 << (i)))
 #define PopBit(b, i) ((b) &= ~(1ULL << (i)))
+
+extern U64 PrecomputedPawnAttacks[2][64];
+extern U64 PrecomputedPawnMoves[2][64];
+extern U64 PrecomputedKnightMoves[64];
+extern U64 PrecomputedKingMoves[64];
+
+U64 PawnAttackBitboard(U64 PawnPos, int Color);
+U64 PawnMoveBitboard(U64 PawnPos, int Color);
+U64 KnightMoveBitboard(U64 KnightPos);
+void PrecomputePawnTables(U64 attacks[2][64], U64 moves[2][64]);
+void PrecomputeKnightTables(U64 moves[64]);
+void PrecomputeKingTables(U64 moves[64]);
